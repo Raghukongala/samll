f5f711e055872601d67f3a4d74d890ff75812f8c
Raghukongala <kongalaraghu406@gmail.com>
2026-04-06 22:50:28 +0530

    Update variables.tf

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,10 +1,42 @@
-variable "project"           { type = string }
-variable "env"               { type = string }
-variable "aws_region"        { type = string }
-variable "vpc_id"            { type = string }
-variable "vpc_cidr"          { type = string }
-variable "public_subnet_ids" { type = list(string) }
-variable "private_subnet_ids"{ type = list(string) }
-variable "ecr_base"          { type = string; description = "Base ECR URL, e.g. 123456789.dkr.ecr.ap-south-1.amazonaws.com/myapp/dev" }
-variable "image_tag"         { type = string; default = "latest" }
-variable "tags"              { type = map(string); default = {} }
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "ecr_base" {
+  type        = string
+  description = "Base ECR URL, e.g. 123456789.dkr.ecr.ap-south-1.amazonaws.com/myapp/dev"
+}
+
+variable "image_tag" {
+  type    = string
+  default = "latest"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
