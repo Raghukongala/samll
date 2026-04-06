83c28a484480fa63c75ea63bbedc7077db6dd538
Raghukongala <kongalaraghu406@gmail.com>
2026-04-06 22:54:00 +0530

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,7 +1,28 @@
-variable "project"            { type = string }
-variable "env"                { type = string }
-variable "cidr_block"         { type = string }
-variable "public_subnets"     { type = list(string) }
-variable "private_subnets"    { type = list(string) }
-variable "availability_zones" { type = list(string) }
-variable "tags"               { type = map(string); default = {} }
+variable "project" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
